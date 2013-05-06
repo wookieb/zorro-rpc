87540c833240a83f4aab8cc7e7b2fc74a411b9d4
wookieb <wookieb@wp.pl>
2013-05-06 17:53:26 +0200

    cleaning up

--- client.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'vendor/autoload.php';
-use Wookieb\ZorroRPC\Client\Client;
-
-$client = Client::create('tcp://0.0.0.0:8000');
-
-var_dump($client->ping());
-
--- rep.php
+++
@@ -1,5 +0,0 @@
-<?php
-$socket = new ZMQSocket(new ZMQContext(), ZMQ::SOCKET_REP);
-$socket->bind('tcp://0.0.0.0:4242');
-var_dump($socket->recv());
-sleep(100);
